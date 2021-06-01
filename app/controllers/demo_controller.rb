d5e3edc8c6e2a75ec2ad69f70ff3eeb5658a5217
Prabhakar Kumar <prabhakar1069@gmail.com>
2021-06-01 22:47:07 +0530

    new changes done

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -2,5 +2,6 @@ class DemoController < ApplicationController
   def index
     @this = 5;
     @new  = 7;
+    @var = 5;
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    
+    @val = 7
   end	
 end
